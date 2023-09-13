ea0a87787e77cb309a9d9f857652a57a1b9c6691
Alejandro Valdivia <alejandrovaldivia1@upb.edu>
2023-09-13 13:13:48 -0400

    Splitter v3

--- Splitting.py
+++ Splitting.py
@@ -1,19 +1,20 @@
 from langchain.text_splitter  import RecursiveCharacterTextSplitter, CharacterTextSplitter
 from Load_data import LoadData
 
-data = LoadData().docs
+class Splitter:
 
-chunk_size = 100
-chunk_overlap = 50
-
-r_splitter = RecursiveCharacterTextSplitter( chunk_size = chunk_size, chunk_overlap = chunk_overlap, separators=["\n", "\n\n", " ", ""])
-c_splitter = CharacterTextSplitter( chunk_size = chunk_size, chunk_overlap = chunk_overlap, separator = ' ')
-
-def splitter(data):
-    texts_splitted = []
-    for doc in data:
-        text = c_splitter.split_text(doc[0].page_content)
-        texts_splitted.append(text)
-    return texts_splitted
-
-print(splitter(data))
+    def __init__(self):
+        self.data = LoadData().docs
+        self.chunk_size = 100
+        self.chunk_overlap = 50
+        self.r_splitter = RecursiveCharacterTextSplitter( chunk_size = self.chunk_size, chunk_overlap = self.chunk_overlap, separators=["\n", "\n\n","\n\n" * 3, " ", ""])
+        self.c_splitter = CharacterTextSplitter( chunk_size = self.chunk_size, chunk_overlap = self.chunk_overlap, separator = ' ')
+    
+    def splitter(self, data):
+        texts_splitted = []
+        for doc in data:
+            text = self.c_splitter.split_text(doc[0].page_content)
+            texts_splitted.append(text)
+        return texts_splitted
+#split = Splitter()
+#print(split.splitter(split.data))
