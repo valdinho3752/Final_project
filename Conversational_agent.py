91fdf9b5149bc73f65ed0db49bdc6af12e0934f7
Alejandro Valdivia <alejandrovaldivia1@upb.edu>
2023-09-14 04:01:37 -0400

    conversational agent fixed

--- Conversational_agent.py
+++ Conversational_agent.py
@@ -1,11 +1,11 @@
 from langchain.chains import ConversationalRetrievalChain
 from OpenIA_connection import GPT_connection
 from Vector_store import Vector_Store
-from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
 
 vectorstore = Vector_Store().vectordb
 
-chat = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0), vectorstore.as_retriever())
+chat = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0), vectorstore.as_retriever())
 
 chat_history = []
     
